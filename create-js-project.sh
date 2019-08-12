1432c192659a3cb281ee4e563c576f5cbbf26655
Victor Nunes <victornunes470@gmail.com>
2019-08-12 02:01:07 -0300

    First version

---
+++ create-js-project.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ $# -lt 1 ] ; then 
+   	echo "Please, insert the project name"
+   	exit 1
+fi
+
+FOLDERNAME=$1
+TESTS=$FOLDERNAME/tests
+SRC=$FOLDERNAME/src
+
+INDEX_FILE=index.js
+INDEX_TEST_FILE=index.tests.js
+
+mkdir $FOLDERNAME
+
+mkdir $TESTS && mkdir $SRC
+
+touch $TESTS/$INDEX_TEST_FILE && touch $SRC/$INDEX_FILE
+
+touch $SRC/.gitignore
+
+touch $FOLDERNAME/README.md
+
+cd $FOLDERNAME
+
+git init
+
+npm init --yes
+
+cd ..
+
+echo "Your project was created" 
